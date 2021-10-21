b07e962b786ff4e0c2cfea496768e06648c2295e
tatkuptcova <tatkuptcova@yandex.ru>
2021-10-21 16:15:13 +0200

    скопированы eslintrc, editoconfig, gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'no-console': 'off',
+    'object-curly-newline': ['error', { ImportDeclaration: 'never', ExportDeclaration: 'never' }],
+    'spaced-comment': ['error', 'always'],
+    'prefer-destructuring': ['error', {
+      VariableDeclarator: {
+        array: false,
+        object: true,
+      },
+      AssignmentExpression: {
+        array: true,
+        object: false,
+      },
+    }, {
+      enforceForRenamedProperties: false,
+    }],
+  },
+};
